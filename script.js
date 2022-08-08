503e8b2b024e651225ab609643599ff899aafcba
hernandez-g <g5hernandez@yahoo.com>
2022-08-07 22:16:54 -0700

    quotes

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ fetch('https://www.officeapi.dev/api/quotes/random')
 });
 
 function displayData(data) {
-    const theQuote = data.data.character.firstname
-    console.log(theQuote);
+    const theMeta = data.data
+    console.log(theMeta);
+    const dataDiv = document.getElementById("data");
+
+
+    // First Name
+
+    const firstName = data.data.character.firstname;
+    console.log(data.data.character.firstname)
+
+    const lastName = data.data.character.lastname;
+    console.log(data.data.character.lastname)
+
+    
+    const quote = data.data.content;
+    console.log(data.data.content)
+    const button = document.createElement("p");
+    button.innerHTML = quote;
+    dataDiv.appendChild(button)
 }
+
+    
